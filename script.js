c844eac1e5c73aea9736837cb0e0df1025b8f9ef
arinahotinskaya <arinakhotinskaya@mail.ru>
2024-11-25 11:27:37 +0300

    Create skills section

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function loadSection(sectionName, containerId) {
   fetch(sectionName)
     .then((response) => response.text())
-    .then((html) => (document.getElementById(containerId).innerHTML = html));
+    .then((html) => (document.getElementById(containerId).innerHTML += html));
 }
 
 loadSection("./sections/header/header.html", "page__header");
 loadSection("./sections/home-section/home-section.html", "page__main");
+loadSection("./sections/skills-section/skills-section.html", "page__main");
